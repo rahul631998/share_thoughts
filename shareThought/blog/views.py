d3c5a30c47944a04d71b8cacabb18a52fc88e195
rahul631998 <rahulchaudhary631998@gmail.com>
2020-07-26 23:02:28 +0530

    Added comment_approve method

--- shareThought/blog/views.py
+++ shareThought/blog/views.py
@@ -77,3 +77,9 @@ def add_comment_to_post(request, pk):
     return render(request, 'blog/comment_form.html', {'form': form})
 
 
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
