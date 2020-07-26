9e0738ca16db9b8cde28efb652bb86e9f356bec6
rahul631998 <rahulchaudhary631998@gmail.com>
2020-07-26 18:53:37 +0530

    dded PostList view

--- shareThought/blog/urls.py
+++ shareThought/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^about/$',views.AboutView.as_view(),name='about'),
-    
+
 ]
--- shareThought/blog/views.py
+++ shareThought/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Post, Comment
+from django.utils import timezone
 
-# Create your views here.
-from django.views.generic import (TemplateView)
+from django.views.generic import (TemplateView, ListView)
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
