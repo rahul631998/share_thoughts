30e56d81cda904253819fa530461fb767c051dcc
rahul631998 <rahulchaudhary631998@gmail.com>
2020-07-26 20:32:23 +0530

    dded CreatePost view

--- shareThought/blog/urls.py
+++ shareThought/blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^about/$',views.AboutView.as_view(),name='about'),
     url(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
-
+    url(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
 ]
--- shareThought/blog/views.py
+++ shareThought/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Post, Comment
+from .forms import PostForm, CommentForm
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from django.views.generic import (TemplateView, ListView, DetailView)
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView)
 
 class AboutView(TemplateView):
     template_name = 'about.html'
@@ -16,4 +19,13 @@ class PostListView(ListView):
 
 
 class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = PostForm
+
     model = Post
